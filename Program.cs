b3f3874a1a0a9104c343c39c10701d0342f3d9d5
Mark Allan <mark@ranyart.com>
2012-08-22 19:07:33 +0100

    Version 1.0

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace LockScreenSaver
+{
+	internal static class Program
+	{
+		[DllImport("user32.dll")]
+		public static extern void LockWorkStation();
+
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			if (args.Length < 1)
+				return;
+
+			var argument = args[0].ToLowerInvariant().Trim();
+
+			if (argument.StartsWith("/c"))
+			{
+				MessageBox.Show(
+					@"This screen saver has no options that you can set.",
+					@"Lock screen saver",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Exclamation);
+			}
+			else if (argument.StartsWith("/s"))
+			{
+				LockWorkStation();
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("LockScreenSaver")]
+[assembly: AssemblyDescription("Simple screensaver that locks the computer")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
